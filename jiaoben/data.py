638fd7552a8d8dd2f4dcd7219c9bdb35be8c97ec
liaofuqian <516955406@qq.com>
2026-05-27 19:09:03 +0800

    新增任务2

--- jiaoben/data.py
+++ jiaoben/data.py
@@ -1,3 +1,6 @@
+from operator import truediv
+
+
 def liebiao():
     '''增删查改
     append,insert,remove,
@@ -92,7 +95,7 @@ def liebiao_renwu1():
     print(f'原始列表数据：{ages}')
     ages1 = []
     for age in ages:
-        if 0 <= age <= 120:
+        if 1 <= age <= 120:
             ages1.append(age)
     ages1.sort()
     print(f'清洗后的列表数据：{ages1}')
@@ -106,10 +109,84 @@ def liebiao_renwu1():
             child_count += 1
     print(f'年成人数量：{adult_count}')
     print(f'未成年人数量：{child_count}')
+    # 每个参数加一个岁字
+    sui_list = []
+    for age in ages1:
+        sui_list.append(f'{age} 岁')
+    print(f'加了岁字：{sui_list}')
+    # 计算平均年龄
+    total = 0
+    for age in ages1:
+        total += age
+    average = total / len(ages1)
+    print(f'平均年龄为：{average:.1f} 岁')
 
+
+def list_requiremen1():
+    '''
+    需求（6个）
+    统计：分别统计"已完成"、"已发货"、"已取消"的订单数量
+    筛选：找出金额大于100元的订单，打印出来
+    求和：计算所有"已完成"订单的总金额
+    转换：生成一个新列表，里面是每个订单的简化信息：订单号-金额元（比如 A001-299元）
+    排序：按金额从高到低排序，打印前3个最贵的订单
+    提取：只提取所有订单号，生成一个单独的列表
+    '''
+    orders = [
+        ["A001", 299, "已发货"],
+        ["A002", 45, "已完成"],
+        ["A003", 1299, "已取消"],
+        ["A004", 89, "已发货"],
+        ["A005", 25, "已完成"],
+        ["A006", 799, "已完成"],
+        ["A007", 59, "已取消"],
+        ["A008", 399, "已发货"],
+    ]
+    print('=' * 50)
+    print('电商订单数据')
+    print('=' * 50)
+    # 需求1，统计各种状态的订单数量
+    status_count = {
+        "已完成": 0,
+        "已发货": 0,
+        "已取消": 0
+    }
+    key = status_count['已完成']
+    print(key)
+    for order in orders:
+        # 确定索引值
+        status = order[2]
+        # 核心：判断这个键是否在字典里面
+        if status in status_count:
+            status_count[status] += 1
+            print(status_count)
+    print("各状态订单数量：")
+    print(f'----- 已完成：{status_count["已完成"]} 单')
+    print(f'----- 已发货：{status_count["已发货"]} 单')
+    print(f'----- 已取消：{status_count["已取消"]} 单')
+
+    #需求2：找出金额大于100元的订单
+    for order in orders:
+        if order[1]>100:
+            print(f'{order[0]}------   {order[1]}   ---{order[2]}')
+    #需求3：计算已完成订单的总金额
+    num=0
+    for order in orders:
+        if order[2]=='已完成':
+            num+=order[1]
+    print(f'已完成订单总金额：{num}')
+    #按金额排序，取前3个最贵的
+    #用sorted排序，key，告诉它按照金额排序，reverse=true，表示从大到小
+    sorted_order=sorted(orders,key=lambda x:x[1],reverse=True)
+    top3=sorted_order[:3]
+    print(top3)
+    print('提取前3个订单')
+    for i,order in enumerate(top3,1):
+        print(f'---第{i}名--{order[0]}--{order[1]}--{order[2]}')
 
 if __name__ == '__main__':
     # liebiao()
     # yuanzu()
     # renwu_liebiao()
-    liebiao_renwu1()
+    # liebiao_renwu1()
+    list_requiremen1()
